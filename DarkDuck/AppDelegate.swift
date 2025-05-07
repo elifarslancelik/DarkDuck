f8f36b83ca4d5625aa66abad95eeb07b06f3b30d
elifarslancelik <elifnurarslnclk@gmail.com>
2025-05-07 10:22:52 +0300

    Initial Commit

---
+++ DarkDuck/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  DarkDuck
+//
+//  Created by Elif Arslancelik on 7.05.2025.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
---
+++ DarkDuckTests/DarkDuckTests.swift
@@ -0,0 +1,17 @@
+//
+//  DarkDuckTests.swift
+//  DarkDuckTests
+//
+//  Created by Elif Arslancelik on 7.05.2025.
+//
+
+import Testing
+@testable import DarkDuck
+
+struct DarkDuckTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
